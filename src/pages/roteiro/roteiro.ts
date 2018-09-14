37e45c35cbacaf361c68781bc50387d9eeb490f3
Unknown <raphaelratuczne@gmail.com>
2018-09-14 16:45:04 -0300

    bloqueia btns na pg roteiro

--- src/pages/roteiro/roteiro.ts
+++ src/pages/roteiro/roteiro.ts
@@ -1,41 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { Dia } from '../../models/dia';
+
+import { DiaProvider } from '../../providers/dia.provider';
 
 @IonicPage()
 @Component({
   selector: 'page-roteiro',
   templateUrl: 'roteiro.html'
 })
-export class RoteiroPage {
-  // selectedItem: any;
-  // icons: string[];
-  // items: Array<{title: string, note: string, icon: string}>;
+export class RoteiroPage implements OnDestroy {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    // this.selectedItem = navParams.get('item');
+  public desPaixao: boolean = true;
+  public desPlano: boolean = true;
+  public desObjetivo: boolean = true;
+  public desOrgulho: boolean = true;
+  public desPalavra: boolean = true;
+  private d$: Subject<void>;
 
-    // Let's populate this page with some filler content for funzies
-    // this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    // 'american-football', 'boat', 'bluetooth', 'build'];
-
-    // this.items = [];
-    // for (let i = 1; i < 11; i++) {
-    //   this.items.push({
-    //     title: 'Item ' + i,
-    //     note: 'This is item #' + i,
-    //     icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-    //   });
-    // }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private diaProvider: DiaProvider) {
+    this.d$ = new Subject();
+    this.diaProvider.getDocDadosDia().pipe(takeUntil(this.d$)).subscribe((dia:Dia) => {
+      // console.log('carregou dia', dia);
+      if (dia) {
+        const isEmpty = val => val.trim() == '';
+        this.desPaixao = (isEmpty(dia.grato1) || isEmpty(dia.grato2) || isEmpty(dia.grato3));
+        this.desPlano = (isEmpty(dia.paixao1) || isEmpty(dia.paixao2) || isEmpty(dia.paixao3) || this.desPaixao);
+        this.desObjetivo = (isEmpty(dia.plano1) || isEmpty(dia.plano2) || isEmpty(dia.plano3) || this.desPlano);
+        this.desOrgulho = (isEmpty(dia.objetivo) || this.desObjetivo);
+        this.desPalavra = isEmpty(dia.orgulho) || this.desOrgulho;
+      }
+    });
   }
 
-  // itemTapped(event, item) {
-  //   // That's right, we're pushing to ourselves!
-  //   this.navCtrl.push('ListPage', {
-  //     item: item
-  //   });
-  // }
+  ngOnDestroy() {
+    this.d$.next();
+    this.d$.complete();
+  }
 
   public goItensPage(pg:string) {
     this.navCtrl.push('ItensPage', { page: pg });
