d071860d5974badd4c7fb8d82072c503a5aff0b5
Unknown <raphaelratuczne@gmail.com>
2018-12-03 14:07:17 -0200

    correção notificações

--- src/providers/local-notification.provider.ts
+++ src/providers/local-notification.provider.ts
@@ -11,21 +11,18 @@ export class LocalNotificationProvider {
   // seta lembretes para os proximos 7 dias
   public setAlertasUsarApp() {
     if (this.platform.is('cordova')) {
-      cordova.plugins.notification.local.cancel([101,102,103,104,105,106,107], () => {
-        let agenda = [];
-        let hj = new Date();
-        hj.setHours(6);
-        hj.setMinutes(0);
-        hj.setSeconds(0);
-        for (let i = 101; i <= 107; i++) {
-          agenda.push({
-            id: i,
-            title: 'Minha Terapia',
-            text: 'Bom dia, não esqueça de fazer sua terapia hoje.',
-            at: new Date( hj.getTime() + ((i-100) * 1000 * 60 * 60 * 23) )
-          });
-        }
-        cordova.plugins.notification.local.schedule(agenda);
+      let hj = new Date();
+      hj.setHours(6);
+      hj.setMinutes(0);
+      hj.setSeconds(0);
+      const amanha = new Date( hj.getTime() + (1000 * 60 * 60 * 24) );
+
+      cordova.plugins.notification.local.schedule({
+        id: 99,
+        title: 'Minha Terapia',
+        text: 'Bom dia, não esqueça de fazer sua terapia hoje.',
+        firstAt: amanha,
+        every: 'day'
       });
     }
   }
